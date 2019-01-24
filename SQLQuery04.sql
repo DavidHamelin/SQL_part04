b4225aac1ed41effa9337eefd1e53938666c76e9
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-24 09:13:22 +0100

    correction

---
+++ SQLQuery04.sql
@@ -0,0 +1,28 @@
+--SQL - Partie 4 : Insertion de données
+
+/*
+Ex 01 :
+INSERT INTO [dbo].[languages] (language, version) VALUES
+('JavaScript', '5'),
+('PHP', '5.2'),
+('PHP', '5.4'),
+('HTML', '5.1'),
+('JavaScript', '6'),
+('JavaScript', '7'),
+('JavaScript', '8'),
+('PHP', '7');
+
+SELECT *
+FROM languages;
+
+ex 02 : 
+Clic droit sur la table : Modifier les 200 lignes du haut
+ou...
+INSERT INTO [dbo].[frameworks] (framework, version) VALUES 
+('Symfony', 'version 2.8'),
+('Symfony', 'version 3'),
+('Jquery', 'version 1.6'),
+('Jquery', 'version 2.10');
+
+SELECT *
+FROM frameworks;*/
